fbd122b55c863b3a51ad0caf627887757a0f5db7
Patrick Möller <patrick_moeller@live.de>
2021-10-20 10:45:47 +0200

    Adding New rules to eslint

--- cards.js
+++ cards.js
@@ -1,26 +1,26 @@
-import { getAllElements } from "./functions.js";
-import { getElement } from "./functions.js";
+import { getAllElements } from './functions.js'
+import { getElement } from './functions.js'
 
-const cards = getAllElements("card");
+const cards = getAllElements('card')
 
 export function toggleAnswersAndBookmarks() {
-  cards.forEach((card) => {
+    cards.forEach((card) => {
     /*Area for toggling Bookmark*/
-    const bookmark = getElement("bookmark", card);
-    bookmark.addEventListener("click", () => {
-      bookmark.classList.toggle("bookmark__active");
-    });
+        const bookmark = getElement('bookmark', card)
+        bookmark.addEventListener('click', () => {
+            bookmark.classList.toggle('bookmark__active')
+        })
 
-    /*Area for toggling Answer-Buttons */
-    const button = getElement("toggle-answer", card);
-    const answer = getElement("answer", card);
-    const textShow = getElement("show", card);
-    const textHide = getElement("hide", card);
+        /*Area for toggling Answer-Buttons */
+        const button = getElement('toggle-answer', card)
+        const answer = getElement('answer', card)
+        const textShow = getElement('show', card)
+        const textHide = getElement('hide', card)
 
-    button.addEventListener("click", () => {
-      answer.classList.toggle("hidden");
-      textShow.classList.toggle("hidden");
-      textHide.classList.toggle("hidden");
-    });
-  });
+        button.addEventListener('click', () => {
+            answer.classList.toggle('hidden')
+            textShow.classList.toggle('hidden')
+            textHide.classList.toggle('hidden')
+        })
+    })
 }
